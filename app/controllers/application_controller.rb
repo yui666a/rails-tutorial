9439cecd1ced9ecb54126b6ec655d7071fe36096
aiso-hitoshi <h.aiso@buysell-technologies.com>
2023-03-02 10:44:50 +0900

    :heavy_check_mark: chap.1 has done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "hello world ¥n hola, mundo!"
+    end
+
+    def goodbey
+        render html: "goodbye, world!"
+    end
 end
